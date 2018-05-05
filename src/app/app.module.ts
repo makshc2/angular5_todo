68996e3b8e275d3ea5bbdb9094df41cc9529b55b
makshc2 <makshc2@gmail.com>
2018-05-05 12:20:42 +0300

    todo start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FormComponent } from './form/form.component';
 import { ListComponent } from './list/list.component';
 import { ListItemComponent } from './list-item/list-item.component';
 import { JsonplaceholderService } from './services/jsonplaceholder.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { JsonplaceholderService } from './services/jsonplaceholder.service';
     ListItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [JsonplaceholderService],
   bootstrap: [AppComponent]
---
+++ src/app/services/jsonplaceholder.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Task } from '../models/Task';
+
+@Injectable()
+export class JsonplaceholderService {
+
+
+  configUrl = 'https://jsonplaceholder.typicode.com/todos/';
+
+  constructor(
+    public  http: HttpClient
+  ) { }
+
+    getTasks() {
+      return this.http.get(this.configUrl);
+    }
+}
