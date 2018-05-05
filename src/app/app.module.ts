f19172152b3d0977bc43a252ab46e5ce523f306c
makshc2 <makshc2@gmail.com>
2018-05-05 11:54:17 +0300

    todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
+import { ListComponent } from './list/list.component';
+import { ListItemComponent } from './list-item/list-item.component';
+import { JsonplaceholderService } from './services/jsonplaceholder.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FormComponent,
+    ListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [JsonplaceholderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
